82b70d550b5a7d6edc17175d9583c4c9f68f7060
Jaden Toon <jaden.toon@gmail.com>
2025-07-16 14:17:37 +0100

    Added entries to a pandas dataframe.

--- analyse-log.py
+++ analyse-log.py
@@ -5,12 +5,12 @@ from user_agents import parse
 log_file = 'sample-log.log'
 
 log_pattern = re.compile(
-    r'(?P<ip>\S+) - (?P<country>[A-Z]{2}) - \[(?P<timestamp>.*?)\] '  # note space at end
-    r'"(?P<method>\S+) (?P<path>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+) '  # space at end
+    r'(?P<ip>\S+) - (?P<country>[A-Z]{2}) - \[(?P<timestamp>.*?)\] '
+    r'"(?P<method>\S+) (?P<path>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+) ' 
     r'"-" "(?P<useragent>.*?)" (?P<responsetime>\d+)'
 )
 
-count = 0
+data = []
 
 with open(log_file, 'r') as f:
     for line in f:
@@ -20,6 +20,7 @@ with open(log_file, 'r') as f:
             ua = parse(entry['useragent'])
             is_bot = ua.is_bot
             browser = ua.browser.family
-            if count == 0:
-                print(f"User-Agent: {ua}\nIs bot: {is_bot}\nBrowser: {browser}")
-                count += 1
+            data.append(entry)
+
+df = pd.DataFrame(data)
+print(df.head(30))
