b5bb1245a2766523bf085ee871e3debf29e25991
Jaden Toon <jaden.toon@gmail.com>
2025-07-17 10:07:00 +0100

    Added querys.

--- analyse-log.py
+++ analyse-log.py
@@ -18,9 +18,34 @@ with open(log_file, 'r') as f:
         if match:
             entry = match.groupdict()
             ua = parse(entry['useragent'])
-            is_bot = ua.is_bot
-            browser = ua.browser.family
+            entry["is_bot"] = ua.is_bot
+            entry["browser"] = ua.browser.family
             data.append(entry)
 
 df = pd.DataFrame(data)
-print(df.head(30))
+
+#Top IP addresses
+print("\nTop 5 IP addresses by request counts:")
+print(df["ip"].value_counts().head(5))
+
+#Top Nations
+print("\nTop 5 Nations by request counts:")
+print(df["country"].value_counts().head(5))
+
+#Top Status Codes that are not 200.
+print("\nTop Status Codes that are not 200 OK")
+print(df[df["status"] != "200"]["status"].value_counts())
+
+#Getting error status codes
+df["status"] = df["status"].astype(int)
+error_logs = df[df["status"].between(400, 599)]
+
+#Top Nations from error codes
+print("\nTop 5 Nations by error logs request counts:")
+print(error_logs["country"].value_counts().head(5))
+
+#Top IP addresses
+print("\nTop 5 IP addresses by error logs request counts:")
+print(error_logs["ip"].value_counts().head(5))
+
+print("\nLength - " +str(len(df.index)))
